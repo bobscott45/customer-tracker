e4c7a2d2bdca81c55d5c22ee5956398b188a1354
Bob <bob@rwscott.co.uk>
2019-03-16 12:03:11 +0000

    implement customerdao

--- src/main/java/dev/bobscott/customertracker/controller/CustomerController.java
+++ src/main/java/dev/bobscott/customertracker/controller/CustomerController.java
@@ -1,13 +1,25 @@
 package dev.bobscott.customertracker.controller;
 
+import dev.bobscott.customertracker.dao.CustomerDAO;
+import dev.bobscott.customertracker.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+    @Autowired
+    private CustomerDAO customerDAO;
+
     @RequestMapping("/list")
-    public String test() {
+    public String listCustomers(Model model) {
+        List<Customer> customers = customerDAO.getCustomers();
+        model.addAttribute("customers", customers);
         return "list-customers";
     }
 }
--- src/main/java/dev/bobscott/customertracker/dao/CustomerDAOImpl.java
+++ src/main/java/dev/bobscott/customertracker/dao/CustomerDAOImpl.java
@@ -6,10 +6,12 @@ import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.datasource.embedded.DataSourceFactory;
+import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 import java.util.List;
 
+@Repository
 public class CustomerDAOImpl implements CustomerDAO {
 
     @Autowired
